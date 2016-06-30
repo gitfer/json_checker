1adaa056162cbde80645328edf86d296cf8352a5
Federico Rota <federico.rota01@gmail.com>
2016-06-30 11:23:37 +0200

    Linting

--- index.js
+++ index.js
@@ -1,112 +1,119 @@
 var glob = require('glob'),
-		_ = require('lodash'),
-		fs = require('fs'),
-		async = require('async');
+    _ = require('lodash'),
+    fs = require('fs'),
+    async = require('async');
 
 var program = require('commander');
 
 var config = require('./config');
-var filePath = config.json_file_path ||'test/*.json';
+var filePath = config.json_file_path || 'test/*.json';
 
 console.log('Reading path... ' + filePath);
-function init(commandName){
-		
-	glob(filePath, null, function(er, files) {
-		var bufferedTranslations = [];
-		console.log('Found ' + files.length + ' files');
-		async.eachSeries(files, function(file, callback) {
 
-			fs.readFile(file, 'utf8', function(err, data) {
-				if (err) {
-					throw err;
-				}
-				try {
+function init(commandName) {
 
-					var items = JSON.parse(data.toString().trim());
-					console.log('Items in file ' + file  + ': ' + items.length);
-					// async.eachSeries(items, function(item) {
-					_.forEach(items, function(item) {
-						item.file = file;
-						bufferedTranslations.push(item);
-					});
-					  callback(null, !err);
+    glob(filePath, null, function(er, files) {
+        var bufferedTranslations = [];
+        console.log('Found ' + files.length + ' files');
+        async.eachSeries(files, function(file, callback) {
 
-				}catch (err) {
-					console.log('File ' + file);
-				}
+            fs.readFile(file, 'utf8', function(err, data) {
+                if (err) {
+                    throw err;
+                }
+                try {
 
-			});
-		}, function(err) {
-			 if (err) {
-				console.log('err', err);
-				throw err;
-			 }
-			console.log('Total Nr. of items: ',bufferedTranslations.length);
+                    var items = JSON.parse(data.toString().trim());
+                    console.log('Items in file ' + file + ': ' + items.length);
+                    // async.eachSeries(items, function(item) {
+                    _.forEach(items, function(item) {
+                        item.file = file;
+                        bufferedTranslations.push(item);
+                    });
+                    callback(null, !err);
 
+                } catch (err) {
+                    console.log('File ' + file);
+                }
 
-			var findConflicts = function() {
-				var translations = _.cloneDeep(bufferedTranslations);
-				var result = _.filter(translations, function(translation) {
-					var conflicts =  _.some(bufferedTranslations, function(item){
-						return item.file !== translation.file && translation.chiaveUi === item.chiaveUi && translation.chiave === item.chiave;
-					});
+            });
+        }, function(err) {
+            if (err) {
+                console.log('err', err);
+                throw err;
+            }
+            console.log('Total Nr. of items: ', bufferedTranslations.length);
+            var result = 'Command not found';
 
-					return conflicts;
-				});
+            var findConflicts = function() {
+                var translations = _.cloneDeep(bufferedTranslations);
+                var result = _.filter(translations, function(translation) {
+                    var conflicts = _.some(bufferedTranslations, function(item) {
+                        return item.file !== translation.file && translation.chiaveUi === item.chiaveUi && translation.chiave === item.chiave;
+                    });
 
-				result = _.sortBy(result, 'chiave');
+                    return conflicts;
+                });
 
-				return _.groupBy(result, 'chiaveUi');
-			};
+                result = _.sortBy(result, 'chiave');
 
-			var uiConflicts = function() {
-				var translations = _.cloneDeep(bufferedTranslations);
-				var result = _.filter(translations, function(translation) {
-					var conflicts =  _.some(bufferedTranslations, function(item){
-						return item.file !== translation.file && translation.chiaveUi === item.chiaveUi && translation.chiave !== item.chiave;
-					});
+                return _.groupBy(result, 'chiaveUi');
+            };
 
-					return conflicts;
-				});
+            var uiConflicts = function() {
+                var translations = _.cloneDeep(bufferedTranslations);
+                var result = _.filter(translations, function(translation) {
+                    var conflicts = _.some(bufferedTranslations, function(item) {
+                        return item.file !== translation.file && translation.chiaveUi === item.chiaveUi && translation.chiave !== item.chiave;
+                    });
 
-				return _.groupBy(result, 'chiaveUi');
-			};
+                    return conflicts;
+                });
 
-			var keyConflicts = function() {
-				var translations = _.cloneDeep(bufferedTranslations);
-				var result = _.filter(translations, function(translation) {
-					var conflicts =  _.some(bufferedTranslations, function(item){
-						return item.file !== translation.file && translation.chiaveUi !== item.chiaveUi && translation.chiave === item.chiave;
-					});
+                return _.groupBy(result, 'chiaveUi');
+            };
 
-					return conflicts;
-				});
+            var keyConflicts = function() {
+                var translations = _.cloneDeep(bufferedTranslations);
+                var result = _.filter(translations, function(translation) {
+                    var conflicts = _.some(bufferedTranslations, function(item) {
+                        return item.file !== translation.file && translation.chiaveUi !== item.chiaveUi && translation.chiave === item.chiave;
+                    });
 
-				return _.groupBy(result, 'chiave');
-			};
+                    return conflicts;
+                });
 
-			if(commandName === 'same'){
-				var result = findConflicts();
-				console.log('Conflicts', result );	
-			}else if(commandName === 'ui'){
-				var result = uiConflicts();
-				console.log('Same UI key', result);
-			}else if(commandName === 'key'){
-				var result = keyConflicts();
-				console.log('Same key', result);
-			}else{
-				console.log('Command not found');
-			}
-			
-		});
+                return _.groupBy(result, 'chiave');
+            };
 
-	});		
-};
+            if (commandName === 'same') {
+                result = findConflicts();
+                console.log('Conflicts', result);
+            } else if (commandName === 'ui') {
+                result = uiConflicts();
+                console.log('Same UI key', result);
+            } else if (commandName === 'key') {
+                result = keyConflicts();
+                console.log('Same key', result);
+            } else {
+                console.log(result);
+            }
+
+        });
+
+    });
+}
 
 
 program
-  .version('0.0.1')
-  .option('-s, --same', 'Same keys', function(){init('same');})
-  .option('-u, --ui', 'Same chiaveUi, different chiave', function(){init('ui');})
-  .option('-k, --key', 'Same chiave, different chiaveUi', function(){init('key');})
-  .parse(process.argv);
+    .version('0.0.1')
+    .option('-s, --same', 'Same keys', function() {
+        init('same');
+    })
+    .option('-u, --ui', 'Same chiaveUi, different chiave', function() {
+        init('ui');
+    })
+    .option('-k, --key', 'Same chiave, different chiaveUi', function() {
+        init('key');
+    })
+    .parse(process.argv);
